37950b61cd70f01e15b7b3b902506ac031d741b5
Abdelrahmann-Abdelrassoul <s-abdelrahman.abdelrassoul@zewailcity.edu.eg>
2026-03-07 16:27:19 +0200

    edit userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { register, login } from "../controllers/authController.js";
+import { getMe, updateMe, getUserById } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 import { validate } from "../middleware/validateMiddleware.js";
-import { registerSchema, loginSchema } from "../utils/validators.js";
+import { updateMeSchema } from "../utils/validators.js";
 
 const router = express.Router();
 
-router.post("/register", validate(registerSchema), register);
-router.post("/login", validate(loginSchema), login);
+router.get("/me", protect, getMe);
+router.patch("/updateMe", protect, validate(updateMeSchema), updateMe);
+router.get("/:id", getUserById);
 
 export default router;
